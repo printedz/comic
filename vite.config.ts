d9ac28cf96954928577627dba58f77e43fb6de95
Alejandro RN <alejandro.regidor.nova@hotmail.com>
2026-01-22 20:09:28 +0100

    whoops

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/comic/",
   plugins: [react()],
   server: {
     allowedHosts: ["pedantesque-acknowledgingly-brenden.ngrok-free.dev"],
